9d66d4f8dd90794f04f54817f18e39837d0b40d3
Dipika Choudhary <dipikachoudhary3104@gmail.com>
2023-05-04 11:39:47 +0530

    done

--- Progress Bar & Slider/ViewController.swift
+++ Progress Bar & Slider/ViewController.swift
@@ -9,11 +9,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var progressBar: UIProgressView!
+    @IBOutlet weak var slider: UISlider!
+    @IBOutlet weak var sliderLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        progressBar.progress = 0.0
+        sliderLabel.text = "0"
     }
 
-
+    @IBAction func savetButton(_ sender: UIButton) {
+        
+        var time = Timer()
+        var a : Float = 0.0
+       
+        time = Timer.scheduledTimer(withTimeInterval: 0.05,repeats: true, block: { time in
+            
+            a+=0.01
+            self.progressBar.progress = a
+            if self.progressBar.progress == 1.0
+            {
+                self.progressBar.progress = 0.0
+            }
+        })
+    }
+    
+    
+    @IBAction func sliderAction(_ sender: UISlider) {
+        sliderLabel.text = Int(slider.value).description
+    }
 }
 
